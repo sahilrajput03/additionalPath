3505b03a989b71f6a417a57910a0a5c7d5c4a90f
Sahil Rajput <sahilrajput03@gmail.com>
2020-08-31 22:23:53 +0530

    Add nr for npm run global shorthand for global executable. ~sahil

--- apc.bat
+++ apc.bat
@@ -1,3 +1,5 @@
 @echo off
+echo Alert : Current working directory changes in command prompt.
+c:
 cd C:\additionalPath
 code .
---
+++ nr.bat
@@ -0,0 +1,4 @@
+@echo off
+echo.
+echo Executing: npm run %*
+npm run %*
