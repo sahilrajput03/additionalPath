82055358c88cec83d15a5dc7659442e12828311e
Sahil Rajput <sahilrajput03@gmail.com>
2021-02-16 23:42:52 +0530

    ~sahil

---
+++ fastBackup/Python projects/ot-todo-app/src/repositories/user_repository.py
@@ -0,0 +1,54 @@
+from entities.user import User
+from database_connection import get_database_connection
+
+
+def get_user_by_row(row):
+    return User(row['username'], row['password']) if row else None
+
+
+class UserRepository:
+    def __init__(self, connection):
+        self.connection = connection
+
+    def find_all(self):
+        cursor = self.connection.cursor()
+
+        cursor.execute('select * from users')
+
+        rows = cursor.fetchall()
+
+        return list(map(get_user_by_row, rows))
+
+    def find_by_username(self, username):
+        cursor = self.connection.cursor()
+
+        cursor.execute(
+            'select * from users where username = ?',
+            (username,)
+        )
+
+        row = cursor.fetchone()
+
+        return get_user_by_row(row)
+
+    def create(self, user):
+        cursor = self.connection.cursor()
+
+        cursor.execute(
+            'insert into users (username, password) values (?, ?)',
+            (user.username, user.password)
+        )
+
+        self.connection.commit()
+
+        return user
+
+    def delete_all(self):
+        cursor = self.connection.cursor()
+
+        cursor.execute('delete from users')
+
+        self.connection.commit()
+
+
+user_repository = UserRepository(get_database_connection())
