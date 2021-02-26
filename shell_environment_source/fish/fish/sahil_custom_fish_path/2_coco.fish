f348c73d62a50e13843c81dca278d77a725bad2c
Sahil Rajput <sahilrajput03@gmail.com>
2021-02-26 21:59:25 +0530

    ~sahil

--- shell_environment_source/fish/fish/sahil_custom_fish_path/2_coco.fish
+++
@@ -1,6 +0,0 @@
-# Debugging for seeing if file is actually sourced when config.fish is executed on the first load of the fish, yikes!!
-# echo "reached here - file - 2_coco.fish"
-
-function coco2
-   ls 
-end
--- shell_environment_source/fish/fish/sahil_custom_fish_path/3_mdc.fish
+++
@@ -1,5 +0,0 @@
-# echo "reached here - 3_mdc.fish file"
-function mdc
-    mkdir $argv
-    cd $argv
-end
