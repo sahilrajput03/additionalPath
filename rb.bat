f1e7a34457ea95f825fb076cd72a06719035d030
Sahil Rajput <sahilrajput03@gmail.com>
2022-05-12 01:37:13 +0530

    purge more

--- rb.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-mkdir rb >nul 2>nul
-xcopy C:\additionalPath\ReactBoilerplate rb /e /i /y >nul 2>nul
-echo.
-echo The far you go, less you know.
-cd rb
-mylinker && echo Starting React app && code .
-:: cd ..
